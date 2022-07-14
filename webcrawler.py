46e12f54cad4d8f935fcac1efea01eb40a9d5368
theuppercaseguy <saadan06@gmail.com>
2022-07-14 15:19:10 +0500

    some modifications made to the output EXCELL

--- webcrawler.py
+++ webcrawler.py
@@ -3,20 +3,22 @@ import requests
 from bs4 import  BeautifulSoup
 import argparse
 from openpyxl import Workbook
+import openpyxl
 
 data= []
 
-def yelp_spider(max_pages=0,URL=""):
+def yelp_spider(max_pages=0,URL=''):
    
    page = 0
    pages = []
-   url = URL.replace('\n','')
+   print('\n')
+
    while page <= max_pages:
-      pages.append(requests.get(url + '&start='+str(page*10)))
+      pages.append(requests.get(URL + '&start='+str(page*10)))
       print('page: ',page+1,' scanned')
       page+=1
    
-   print('\n\n')
+   print('\n')
    page=0
    while page<=max_pages:
       
@@ -27,16 +29,15 @@ def yelp_spider(max_pages=0,URL=""):
       for link in soup.findAll('a',{'class':'css-1m051bw'}):
          title = link.string
          href = 'https://www.yelp.com' + link.get('href')
-         if it < 2:##first 2 are always adds
+         if it < 3:  ##first 2 are always adds
             it+=1
             continue
          print('title: ',title)
          get_inside_data(href,title)
-      
+           
       page+=1
       print('page: ',page,' Done\n')
-   
-   
+
 def get_inside_data(item_url,title):
    
    page_source_code = requests.get(item_url)
@@ -75,28 +76,32 @@ def get_inside_data(item_url,title):
    
    print("\n###################################################\n")
 
-
-if __name__ =='__main__':
-   system('color 0b')
+def args_parsed():
    parser = argparse.ArgumentParser()
    
    parser.add_argument('-u',type=str,help='(Required): Enter the URL of the searched Buisnessess to start parsing.',required=True)
    
    parser.add_argument('-p',type=int,default='1',help='(Optional): Is for how many pages you want to scrap (Default: 1),i.e: -p 2, will scan the firest 2 pages of a given link')
    
-   parser.add_argument('-o',type=str ,help='(Optional): Used to export the collected data to excell file.',required=False)
+   parser.add_argument('-o',type=str ,default='file.xlsx',help='(Optional): Used to export the collected data to excell file.',required=False)
    
    args = parser.parse_args()
-   
-   yelp_spider(args.p - 1,args.u)
-   
-   if args.o:
-      wb = Workbook() # creates a workbook object.
-      ws = wb.active # creates a worksheet object.
+   return args
 
-      for row in data:
-         ws.append(row) # adds values to cells, each list is a new row.
-      wb.save(args.o) # save to excel file.
-      print('Output produce as: ',args.o)
+def save_data():
+   wb = Workbook() # creates a workbook object.
+   ws = wb.active # creates a worksheet object.
+   ws.append(['TITLE OF BUISNESS','PHONE NUMBER','LOCATION','WEBSITE'])
+   for row in data:
+      ws.append(row) # adds values to cells, each list is a new row.
+   wb.save(filename = args_parsed().o) # save to excel file.
+   print('Output produce as: ',args_parsed().o)
+
+if __name__ =='__main__':
+   system('color 0b')
+   yelp_spider(args_parsed().p - 1,args_parsed().u)
+   
+   if args_parsed().o:
+      save_data()
       
    print('\n\nby: The UPPERCASE GUY.\n\n')
